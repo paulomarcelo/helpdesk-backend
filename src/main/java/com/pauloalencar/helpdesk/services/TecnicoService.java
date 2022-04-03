467e264d6a4d30a27459a08b6cc5d731d6b3f9f4
paulomarcelo <paulo.marcelo@gmail.com>
2022-04-03 17:20:15 -0300

    Exceção para objeto não encontrado

--- src/main/java/com/pauloalencar/helpdesk/services/TecnicoService.java
+++ src/main/java/com/pauloalencar/helpdesk/services/TecnicoService.java
@@ -7,15 +7,16 @@ import org.springframework.stereotype.Service;
 
 import com.pauloalencar.helpdesk.domain.Tecnico;
 import com.pauloalencar.helpdesk.repositories.TecnicoRepository;
+import com.pauloalencar.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
