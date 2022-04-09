2e4872e53e7531835570ac89f94f20dd42623d9f
paulomarcelo <paulo.marcelo@gmail.com>
2022-04-09 16:58:40 -0300

    Validacao nos campos de criacao de novo usuario

---
+++ src/main/java/com/pauloalencar/helpdesk/resources/exceptions/ValidarionError.java
@@ -0,0 +1,26 @@
+package com.pauloalencar.helpdesk.resources.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidarionError extends StandardError{
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidarionError() {
+		super();
+	}
+
+	public ValidarionError(Long timestamp, Integer status, String error, String message, String path) {
+		super(timestamp, status, error, message, path);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		this.errors.add(new FieldMessage(fieldName, message));	
+	}
+}
