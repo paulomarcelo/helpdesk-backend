d7a7398adfd277ab7366b4a05e71d2c4c1e80a7d
paulomarcelo <paulo.marcelo@gmail.com>
2022-04-21 16:32:13 -0300

    create para Chamado

--- src/main/java/com/pauloalencar/helpdesk/services/ChamadoService.java
+++ src/main/java/com/pauloalencar/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.pauloalencar.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pauloalencar.helpdesk.domain.Chamado;
+import com.pauloalencar.helpdesk.domain.Cliente;
+import com.pauloalencar.helpdesk.domain.Tecnico;
+import com.pauloalencar.helpdesk.domain.dtos.ChamadoDTO;
+import com.pauloalencar.helpdesk.domain.enums.Prioridade;
+import com.pauloalencar.helpdesk.domain.enums.Status;
 import com.pauloalencar.helpdesk.repositories.ChamadoRepository;
 import com.pauloalencar.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -15,7 +22,11 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
-	
+	@Autowired
+	TecnicoService tecnicoService;
+	@Autowired
+	ClienteService clienteService;
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
@@ -24,4 +35,27 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		return repository.findAll();
 	}
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+
+		Chamado chamado = new Chamado();
+		if (obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+	}
+
 }
